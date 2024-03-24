cb5f19d186af446a04d4397611fb7819b4fed56a
Kar Petrosyan <92274156+karpetrosyan@users.noreply.github.com>
2024-03-24 20:37:10 +0400

    Fix ssh urls with trailing `.git` (#24)

--- src/rooster/_github.py
+++ src/rooster/_github.py
@@ -115,14 +115,13 @@ def parse_remote_url(remote_url: str) -> tuple[str, str]:
     if remote_url.startswith(ssh_prefix):
         owner_slash_repo = remote_url[len(ssh_prefix) :]
         owner, repo = owner_slash_repo.split("/")
-        return owner, repo
     else:
         parts = remote_url.split("/")
         owner = parts[-2]
         repo = parts[-1]
-        if repo.endswith(".git"):
-            repo = repo[:-4]
-        return owner, repo
+    if repo.endswith(".git"):
+        repo = repo[:-4]
+    return owner, repo
 
 
 def get_pull_requests_for_commits(
