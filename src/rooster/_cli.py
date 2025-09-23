55d0549b4b52ba13ebdab6f8ce7da672d689854b
Zanie Blue <contact@zanie.dev>
2025-09-23 09:47:13 -0500

    Fix inspecting repository log

--- src/rooster/_cli.py
+++ src/rooster/_cli.py
@@ -60,7 +60,7 @@ def release(
         raise typer.Exit(1)
 
     # Get the last release version
-    typer.echo(f"Inspecting repository `{directory.name}`")
+    typer.echo(f"Inspecting project at {directory.resolve()}")
     repo = repo_from_path(directory)
     version_tags = versions_from_git_tags(config, repo)
     last_version = get_latest_version(version_tags.keys())
